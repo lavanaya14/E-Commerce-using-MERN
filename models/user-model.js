af9fa0ebfdc93cb21a1aa903bf3486d6a7791459
Lavanaya Rajput <149952338+lavanaya14@users.noreply.github.com>
2025-08-01 22:21:58 +0530

    Add files via upload

---
+++ models/user-model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1.27017/first project");
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        minlength: 3,
+        trim: true,
+    },
+    email: String,
+    password: String,
+    cart: {
+        type: Array,
+        default: []
+    },
+    orders: {
+        type: Array,
+        default: [],
+    },
+    contact: Number,
+    picture: String,
+});
+
+module.exports = mongoose.model("user", userSchema);
